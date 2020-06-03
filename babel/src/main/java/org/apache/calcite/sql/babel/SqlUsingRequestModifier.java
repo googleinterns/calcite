6af98437e3ec88d0db5417fae514bbb0c975365b
AndrewPochapsky <andrew.pochapsky@gmail.com>
2020-06-03 10:03:00 -0400

    removed unused import

--- babel/src/main/java/org/apache/calcite/sql/babel/SqlUsingRequestModifier.java
+++ babel/src/main/java/org/apache/calcite/sql/babel/SqlUsingRequestModifier.java
@@ -19,7 +19,6 @@ package org.apache.calcite.sql.babel;
 import org.apache.calcite.jdbc.CalcitePrepare;
 import org.apache.calcite.sql.SqlCall;
 import org.apache.calcite.sql.SqlExecutableStatement;
-import org.apache.calcite.sql.SqlIdentifier;
 import org.apache.calcite.sql.SqlKind;
 import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.SqlNodeList;
