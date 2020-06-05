2bf1063ced93f084ef533464f09a3b859ebfa675
Ezra <eumen@google.com>
2020-06-05 16:11:53 -0700

    [CALCITE] Unparse unicode correctly in BigQuery (#39)
    BQ doesn't support u& unicode literals, but it does support \uXXXX type characters, change to unparse in this way
    
    PiperOrigin-RevId: 314990799

--- core/src/main/java/org/apache/calcite/sql/SqlDialect.java
+++ core/src/main/java/org/apache/calcite/sql/SqlDialect.java
@@ -628,7 +628,7 @@ public class SqlDialect {
     buf.append("'");
   }
 
-  private static final char[] HEXITS = {
+  protected static final char[] HEXITS = {
       '0', '1', '2', '3', '4', '5', '6', '7',
       '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
   };
