6774a52f02248e0d9a072a023c9da1825baa543b
Drew Schmitt <62313599+dasch-1@users.noreply.github.com>
2020-06-22 09:48:25 -0700

    Small spacing and comment fixes. (#88)

--- babel/src/main/java/org/apache/calcite/sql/babel/SqlTranslateUsingCharacterSet.java
+++ babel/src/main/java/org/apache/calcite/sql/babel/SqlTranslateUsingCharacterSet.java
@@ -77,7 +77,7 @@ public class SqlTranslateUsingCharacterSet extends SqlCall implements SqlExecuta
     writer.print(")");
   }
 
-  // Intentionally leave empty
+  // Intentionally left empty.
   @Override public void execute(final CalcitePrepare.Context context) {
   }
 }
