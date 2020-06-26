e6ebd1d20856285f4a1305ec2dab36c24353a209
michaelvdow <michaelvdow@gmail.com>
2020-06-26 11:42:42 -0500

    Copied SQL Java files from server/babel into core (#107)

---
+++ core/src/main/java/org/apache/calcite/sql/SqlCreateTableDialect1.java
@@ -0,0 +1,162 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.calcite.sql;
+
+import org.apache.calcite.jdbc.CalcitePrepare;
+import org.apache.calcite.sql.parser.SqlParserPos;
+import org.apache.calcite.util.ImmutableNullableList;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Parse tree for {@code CREATE TABLE} statement.
+ */
+public class SqlCreateTableDialect1 extends SqlCreate
+    implements SqlExecutableStatement {
+  public final SqlIdentifier name;
+  public final SetType setType;
+  public final Volatility volatility;
+  public final List<SqlCreateAttribute> tableAttributes;
+  public final SqlNodeList columnList;
+  public final SqlNode query;
+  public final WithDataType withData;
+  public final SqlPrimaryIndex primaryIndex;
+  public final OnCommitType onCommitType;
+
+  private static final SqlOperator OPERATOR =
+      new SqlSpecialOperator("CREATE TABLE", SqlKind.CREATE_TABLE);
+
+  /** Creates a SqlCreateTableDialect1. */
+  public SqlCreateTableDialect1(SqlParserPos pos, boolean replace, SetType setType, Volatility volatility,
+      boolean ifNotExists, SqlIdentifier name, SqlNodeList columnList, SqlNode query) {
+    this(pos, replace, setType, volatility, ifNotExists, name, /*tableAttributes=*/null,
+        columnList, query, WithDataType.UNSPECIFIED,
+        /*primaryIndex=*/ null, OnCommitType.UNSPECIFIED);
+  }
+
+  public SqlCreateTableDialect1(SqlParserPos pos, boolean replace, SetType setType, Volatility volatility,
+      boolean ifNotExists, SqlIdentifier name, SqlNodeList columnList, SqlNode query,
+      WithDataType withData, OnCommitType onCommitType) {
+    this(pos, replace, setType, volatility, ifNotExists, name, /*tableAttributes=*/null,
+        columnList, query, withData, /*primaryIndex=*/ null, onCommitType);
+  }
+
+  public SqlCreateTableDialect1(SqlParserPos pos, boolean replace, SetType setType, Volatility volatility,
+      boolean ifNotExists, SqlIdentifier name, List<SqlCreateAttribute> tableAttributes,
+      SqlNodeList columnList, SqlNode query,
+      WithDataType withData, OnCommitType onCommitType) {
+    this(pos, replace, setType, volatility, ifNotExists, name, tableAttributes,
+        columnList, query, withData, /*primaryIndex=*/ null, onCommitType);
+  }
+
+  public SqlCreateTableDialect1(SqlParserPos pos, boolean replace, SetType setType, Volatility volatility,
+      boolean ifNotExists, SqlIdentifier name, List<SqlCreateAttribute> tableAttributes,
+      SqlNodeList columnList, SqlNode query,
+      WithDataType withData, SqlPrimaryIndex primaryIndex, OnCommitType onCommitType) {
+    super(OPERATOR, pos, replace, ifNotExists);
+    this.name = Objects.requireNonNull(name);
+    this.setType = setType;
+    this.volatility = volatility;
+    this.tableAttributes = tableAttributes; // may be null
+    this.columnList = columnList; // may be null
+    this.query = query; // for "CREATE TABLE ... AS query"; may be null
+    this.withData = withData;
+    this.primaryIndex = primaryIndex;
+    this.onCommitType = onCommitType;
+  }
+
+  @Override public List<SqlNode> getOperandList() {
+    return ImmutableNullableList.of(name, columnList, query);
+  }
+
+  @Override public void unparse(SqlWriter writer, int leftPrec, int rightPrec) {
+    writer.keyword("CREATE");
+    switch (setType) {
+    case SET:
+      writer.keyword("SET");
+      break;
+    case MULTISET:
+      writer.keyword("MULTISET");
+      break;
+    default:
+      break;
+    }
+    switch (volatility) {
+    case VOLATILE:
+      writer.keyword("VOLATILE");
+      break;
+    case TEMP:
+      writer.keyword("TEMP");
+      break;
+    default:
+      break;
+    }
+    writer.keyword("TABLE");
+    if (ifNotExists) {
+      writer.keyword("IF NOT EXISTS");
+    }
+    name.unparse(writer, leftPrec, rightPrec);
+    if (tableAttributes != null) {
+      SqlWriter.Frame frame = writer.startList("", "");
+      for (SqlCreateAttribute a : tableAttributes) {
+        writer.sep(",", true);
+        a.unparse(writer, 0, 0);
+      }
+      writer.endList(frame);
+    }
+    if (columnList != null) {
+      SqlWriter.Frame frame = writer.startList("(", ")");
+      for (SqlNode c : columnList) {
+        writer.sep(",");
+        c.unparse(writer, 0, 0);
+      }
+      writer.endList(frame);
+    }
+    if (query != null) {
+      writer.keyword("AS");
+      writer.newlineAndIndent();
+      query.unparse(writer, 0, 0);
+    }
+    switch (withData) {
+    case WITH_DATA:
+      writer.keyword("WITH DATA");
+      break;
+    case WITH_NO_DATA:
+      writer.keyword("WITH NO DATA");
+      break;
+    default:
+      break;
+    }
+    if (primaryIndex != null) {
+      primaryIndex.unparse(writer, leftPrec, rightPrec);
+    }
+    switch (onCommitType) {
+    case PRESERVE:
+      writer.keyword("ON COMMIT PRESERVE ROWS");
+      break;
+    case RELEASE:
+      writer.keyword("ON COMMIT RELEASE ROWS");
+      break;
+    default:
+      break;
+    }
+  }
+
+  // Intentionally left empty.
+  @Override public void execute(CalcitePrepare.Context context) {}
+}
---
+++ core/src/main/java/org/apache/calcite/sql/SqlDateTimeAtLocal.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.calcite.sql;
+
+import org.apache.calcite.jdbc.CalcitePrepare;
+import org.apache.calcite.sql.parser.SqlParserPos;
+import org.apache.calcite.util.ImmutableNullableList;
+
+import java.util.List;
+
+public class SqlDateTimeAtLocal extends SqlCall implements SqlExecutableStatement {
+  public static final SqlSpecialOperator OPERATOR =
+      new SqlSpecialOperator("AT LOCAL", SqlKind.OTHER);
+
+  private final SqlNode dateTimePrimary;
+
+  public SqlDateTimeAtLocal(SqlParserPos pos, SqlNode dateTimePrimary) {
+    super(pos);
+    this.dateTimePrimary = dateTimePrimary;
+  }
+
+  @Override public SqlOperator getOperator() {
+    return OPERATOR;
+  }
+
+  @Override public List<SqlNode> getOperandList() {
+    return ImmutableNullableList.of(dateTimePrimary);
+  }
+
+  @Override public void unparse(SqlWriter writer, int leftPrec, int rightPrec) {
+    dateTimePrimary.unparse(writer, leftPrec, rightPrec);
+    writer.keyword("AT LOCAL");
+  }
+
+  // Intentionally left empty.
+  @Override public void execute(CalcitePrepare.Context context) {}
+}
