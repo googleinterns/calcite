8ebec39240c497ee0a16e287fa8637ac0d366ace
Drew Schmitt <62313599+dasch-1@users.noreply.github.com>
2020-06-30 21:41:31 -0700

    [CALCITE] Move @Override tags to same line as declaration (#123)

--- babel/src/main/java/org/apache/calcite/sql/babel/SqlAlterTableAttributeChecksum.java
+++ babel/src/main/java/org/apache/calcite/sql/babel/SqlAlterTableAttributeChecksum.java
@@ -40,8 +40,7 @@ public class SqlAlterTableAttributeChecksum extends SqlTableAttributeChecksum {
     this.immediate = immediate;
   }
 
-  @Override
-  public void unparse(SqlWriter writer, int leftPrec, int rightPrec) {
+  @Override public void unparse(SqlWriter writer, int leftPrec, int rightPrec) {
     super.unparse(writer, leftPrec, rightPrec);
     if (immediate) {
       writer.keyword("IMMEDIATE");
