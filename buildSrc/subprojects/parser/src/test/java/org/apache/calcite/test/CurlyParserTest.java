googleinterns/calcite

commit 586d027e3edb0cfa6ac4923dea606183cf6c5985

[CALCITE] DialectGenerate can now parse escaped double quotes in Strings and escaped single quotes in characters (#198)

buildSrc/subprojects/parser/src/main/java/org/apache/calcite/buildtools/parser/CurlyParser.java
@@ -64,6 +64,8 @@ public class CurlyParser {
   // legally encountered (those that are not within any structure).
   private int curlyCounter = 0;
 
+  private String previousToken = "";
+
   /**
    * Parses the given token and updates the state. It is assumed that the
    * tokens are provided as a sequence:
@@ -90,13 +92,13 @@ public class CurlyParser {
       if (insideState == InsideState.SINGLE_COMMENT) {
         insideState = InsideState.NONE;
       }
-    } else if (token.equals("\"")) {
+    } else if (token.equals("\"") && !previousToken.equals("\\")) {
       if (insideState == InsideState.NONE) {
         insideState = InsideState.STRING;
       } else if (insideState == InsideState.STRING) {
         insideState = InsideState.NONE;
       }
-    } else if (token.equals("'")) {
+    } else if (token.equals("'") && !previousToken.equals("\\")) {
       if (insideState == InsideState.NONE) {
         insideState = InsideState.CHARACTER;
       } else if (insideState == InsideState.CHARACTER) {
@@ -123,5 +125,6 @@ public class CurlyParser {
         curlyCounter--;
       }
     }
+    previousToken = token;
   }
 }

buildSrc/subprojects/parser/src/test/java/org/apache/calcite/test/CurlyParserTest.java
@@ -66,6 +66,16 @@ public class CurlyParserTest {
     assertCurlyParserSucceeds(input);
   }
 
+  @Test public void testCurlyParserParsesDoubleQuoteInString() {
+    String input = "{ \"abc\\\" \"  }";
+    assertCurlyParserSucceeds(input);
+  }
+
+  @Test public void testCurlyParserParsesSingleQuoteInSingleQuotes() {
+    String input = "{ 'abc\\''  }";
+    assertCurlyParserSucceeds(input);
+  }
+
   @Test public void testCurlyParserParsesNestedValidCurlyBlocks() {
     String input = "{ {  } }";
     assertCurlyParserSucceeds(input);

buildSrc/subprojects/parser/src/test/resources/processFileTests/multiple_functions_separated/multiple_functions_separated_expected.txt
@@ -17,6 +17,7 @@
 void foo(int a) :
 {
     String x = "}";
+    String y = "\"";
 }
 {
     /*
@@ -26,6 +27,7 @@ void foo(int a) :
 void bar( int a, int b ) :
 {
     String x = " ' } ' ";
+    char y = '\'';
 }
 {
     /*

buildSrc/subprojects/parser/src/main/java/org/apache/calcite/buildtools/parser/DialectGenerate.java
@@ -35,7 +35,8 @@ public class DialectGenerate {
   // handling of nested angle brackets and comma separated values.
   private static final String TYPE = "[\\w\\.]+\\s*(<\\s*[\\w<>,\\s]+>)?";
   private static final String TYPE_AND_NAME = TYPE + "\\s+\\w+";
-  private static final String SPLIT_DELIMS = "(\\s|\n|\"|//|/\\*|\\*/|'|\\}|\\{)";
+  // The \\\\ delim splits by \.
+  private static final String SPLIT_DELIMS = "(\\s|\\\\|\n|\"|//|/\\*|\\*/|'|\\}|\\{)";
 
   // Used to split up a string into tokens by the specified deliminators
   // while also keeping the deliminators as tokens.

commit 5ddaae55cb1520ed8b495663ba02ca44511ca3cc

[CALCITE] Fixed bug where CurlyParser would not parse escaped backslashes in a string (#201)
A bug was introduced in #198 which prevented escaped backslashes from being parsed within double quotes.

buildSrc/subprojects/parser/src/main/java/org/apache/calcite/buildtools/parser/CurlyParser.java
@@ -64,7 +64,9 @@ public class CurlyParser {
   // legally encountered (those that are not within any structure).
   private int curlyCounter = 0;
 
-  private String previousToken = "";
+  // Keeps track of the number of consecutive backslashes so it can be
+  // determined if the current token is escaped or not.
+  private int consecutiveBackslashes = 0;
 
   /**
    * Parses the given token and updates the state. It is assumed that the
@@ -92,13 +94,13 @@ public class CurlyParser {
       if (insideState == InsideState.SINGLE_COMMENT) {
         insideState = InsideState.NONE;
       }
-    } else if (token.equals("\"") && !previousToken.equals("\\")) {
+    } else if (token.equals("\"") && !isEscaped()) {
       if (insideState == InsideState.NONE) {
         insideState = InsideState.STRING;
       } else if (insideState == InsideState.STRING) {
         insideState = InsideState.NONE;
       }
-    } else if (token.equals("'") && !previousToken.equals("\\")) {
+    } else if (token.equals("'") && !isEscaped()) {
       if (insideState == InsideState.NONE) {
         insideState = InsideState.CHARACTER;
       } else if (insideState == InsideState.CHARACTER) {
@@ -125,6 +127,20 @@ public class CurlyParser {
         curlyCounter--;
       }
     }
-    previousToken = token;
+    if (token.equals("\\")) {
+      consecutiveBackslashes++;
+    } else {
+      consecutiveBackslashes = 0;
+    }
+  }
+
+  /**
+   * Determines if the current token is escaped based on how many consecutive
+   * backslashes precede it. An even number of backslashes implies the token
+   * is not escaped (as in that case the backslash itself is escaped), while
+   * an odd number implies that the token is escaped.
+   */
+  private boolean isEscaped() {
+    return consecutiveBackslashes % 2 == 1;
   }
 }

buildSrc/subprojects/parser/src/test/java/org/apache/calcite/test/CurlyParserTest.java
@@ -76,6 +76,16 @@ public class CurlyParserTest {
     assertCurlyParserSucceeds(input);
   }
 
+  @Test public void testCurlyParserParsesEscapedBackslashInSingleQuotes() {
+    String input = "{ '\\\\'  }";
+    assertCurlyParserSucceeds(input);
+  }
+
+  @Test public void testCurlyParserParsesEscapedBackslash() {
+    String input = "{ String x = \"\\\\\" }";
+    assertCurlyParserSucceeds(input);
+  }
+
   @Test public void testCurlyParserParsesNestedValidCurlyBlocks() {
     String input = "{ {  } }";
     assertCurlyParserSucceeds(input);

buildSrc/subprojects/parser/src/test/resources/processFileTests/multiple_functions_separated/multiple_functions_separated_expected.txt
@@ -18,6 +18,8 @@ void foo(int a) :
 {
     String x = "}";
     String y = "\"";
+    String z = "\\";
+    char l = '\\';
 }
 {
     /*