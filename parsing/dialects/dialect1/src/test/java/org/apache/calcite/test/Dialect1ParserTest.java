47ae0294c2d453ea37dc8461e993aead39bad735
Arthur Chen <chenarthur@google.com>
2020-07-22 19:36:04 +0000

    testRangeNNoRange fails

--- parsing/dialects/dialect1/src/test/java/org/apache/calcite/test/Dialect1ParserTest.java
+++ parsing/dialects/dialect1/src/test/java/org/apache/calcite/test/Dialect1ParserTest.java
@@ -3502,6 +3502,12 @@ final class Dialect1ParserTest extends SqlDialectParserTest {
     expr(sql).ok(expected);
   }
 
+  @Test public void testRangeNNoRange() {
+    final String sql = "range_n (foo between 3 and 10, no range)";
+    final String expected = "RANGE_N(`FOO` BETWEEN * AND 10, NO RANGE)";
+    expr(sql).ok(expected);
+  }
+
   @Test void testNestedNamedFunctionCalls() {
     final String sql = "SELECT\n"
         + "  MY_FUN(\n"
