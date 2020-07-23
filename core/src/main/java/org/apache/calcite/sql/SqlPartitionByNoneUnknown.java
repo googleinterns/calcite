1d114dc31478b2d899e98d6412106a29ee982712
Arthur Chen <chenarthur@google.com>
2020-07-23 02:43:42 +0000

    Fix format issue

--- core/src/main/java/org/apache/calcite/sql/SqlPartitionByNoneUnknown.java
+++ core/src/main/java/org/apache/calcite/sql/SqlPartitionByNoneUnknown.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.calcite.sql;
 /**
  * SqlPartitionByNoneUnknown defines the possible options at Range_N and Case_N
