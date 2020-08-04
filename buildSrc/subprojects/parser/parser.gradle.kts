8b5400b5c7ce89af7e44ffd96bf37e00c26835ea
AndrewPochapsky <andrew.pochapsky@gmail.com>
2020-08-04 13:50:16 -0400

    removed gson dependency

--- buildSrc/subprojects/parser/parser.gradle.kts
+++ buildSrc/subprojects/parser/parser.gradle.kts
@@ -21,7 +21,6 @@ plugins {
 }
 
 dependencies {
-    implementation("com.google.code.gson:gson:2.8.5")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.1")
 }
