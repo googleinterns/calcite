14c438d288a9d2c173869a995d6f5aef307dd0c3
Andrew Pochapsky <apochapsky@google.com>
2020-08-04 13:08:49 -0400

    [CALCITE] Fixed EXCEPT unparsing bug (#267)
    - Removed overriding of SqlSelect() in BigQuery
    - Override SelectExpression() in BigQuery

--- core/src/main/java/org/apache/calcite/sql/SqlExcept.java
+++ core/src/main/java/org/apache/calcite/sql/SqlExcept.java
@@ -51,7 +51,8 @@ public class SqlExcept extends SqlCall implements SqlExecutableStatement {
   }
 
   @Override public void unparse(SqlWriter writer, int leftPrec, int rightPrec) {
-    writer.keyword("EXCEPT");
+    // This query is only ever valid when preceded by an asterisk.
+    writer.keyword("* EXCEPT");
     SqlWriter.Frame frame = writer.startList("(", ")");
     for (SqlNode e : exceptList) {
       writer.sep(",", false);
