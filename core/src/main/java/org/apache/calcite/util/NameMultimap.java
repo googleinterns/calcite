ef1f1563995293c7b8a7f63bebef5947b5a6f1dc
AndrewPochapsky <andrew.pochapsky@gmail.com>
2020-08-19 15:14:27 -0400

    removed unused import

--- core/src/main/java/org/apache/calcite/util/NameMultimap.java
+++ core/src/main/java/org/apache/calcite/util/NameMultimap.java
@@ -25,7 +25,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.NavigableMap;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import static org.apache.calcite.util.CaseInsensitiveComparator.COMPARATOR;
